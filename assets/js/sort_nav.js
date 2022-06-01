d2121b83df6ff1fe6546dcff21b0caa97fd0776a
Z0dya <vladislavlazutkin64@gmail.com>
2022-06-01 21:41:22 +0300

    final catalog

--- assets/js/sort_nav.js
+++ assets/js/sort_nav.js
@@ -1,34 +1,19 @@
-function myFunction1() {
-    document.getElementById("myDropdown-nav1").classList.toggle("show-nav");
-}
-
-// Close the dropdown if the user clicks outside of it
-window.onclick = function (event) {
-    if (!event.target.matches('.dropbtn-nav')) {
-
-        var dropdowns = document.getElementsByClassName("dropdown-content-nav");
-        var i;
-        for (i = 0; i < dropdowns.length; i++) {
-            var openDropdown = dropdowns[i];
-            if (openDropdown.classList.contains('show-nav')) {
-                openDropdown.classList.remove('show-nav');
-            }
-        }
-    }
-}
-
 function myFunction2() {
     document.getElementById("myDropdown-nav2").classList.toggle("show-nav");
+    document.getElementById("myDropdown-nav3").classList.remove("show-nav");
+    document.getElementById("myDropdown-nav4").classList.remove("show-nav");
+    document.getElementById("myDropdown-nav5").classList.remove("show-nav");
+    document.getElementById("myDropdown-nav6").classList.remove("show-nav");
 }
 
 // Close the dropdown if the user clicks outside of it
 window.onclick = function (event) {
     if (!event.target.matches('.dropbtn-nav')) {
 
-        var dropdowns = document.getElementsByClassName("dropdown-content-nav");
-        var i;
+        let dropdowns = document.getElementsByClassName("dropdown-content-nav");
+        let i;
         for (i = 0; i < dropdowns.length; i++) {
-            var openDropdown = dropdowns[i];
+            let openDropdown = dropdowns[i];
             if (openDropdown.classList.contains('show-nav')) {
                 openDropdown.classList.remove('show-nav');
             }
@@ -38,16 +23,20 @@ window.onclick = function (event) {
 
 function myFunction3() {
     document.getElementById("myDropdown-nav3").classList.toggle("show-nav");
+    document.getElementById("myDropdown-nav2").classList.remove("show-nav");
+    document.getElementById("myDropdown-nav4").classList.remove("show-nav");
+    document.getElementById("myDropdown-nav5").classList.remove("show-nav");
+    document.getElementById("myDropdown-nav6").classList.remove("show-nav");
 }
 
 // Close the dropdown if the user clicks outside of it
 window.onclick = function (event) {
     if (!event.target.matches('.dropbtn-nav')) {
 
-        var dropdowns = document.getElementsByClassName("dropdown-content-nav");
-        var i;
+        let dropdowns = document.getElementsByClassName("dropdown-content-nav");
+        let i;
         for (i = 0; i < dropdowns.length; i++) {
-            var openDropdown = dropdowns[i];
+            let openDropdown = dropdowns[i];
             if (openDropdown.classList.contains('show-nav')) {
                 openDropdown.classList.remove('show-nav');
             }
@@ -57,16 +46,20 @@ window.onclick = function (event) {
 
 function myFunction4() {
     document.getElementById("myDropdown-nav4").classList.toggle("show-nav");
+    document.getElementById("myDropdown-nav2").classList.remove("show-nav");
+    document.getElementById("myDropdown-nav3").classList.remove("show-nav");
+    document.getElementById("myDropdown-nav5").classList.remove("show-nav");
+    document.getElementById("myDropdown-nav6").classList.remove("show-nav");
 }
 
 // Close the dropdown if the user clicks outside of it
 window.onclick = function (event) {
     if (!event.target.matches('.dropbtn-nav')) {
 
-        var dropdowns = document.getElementsByClassName("dropdown-content-nav");
-        var i;
+        let dropdowns = document.getElementsByClassName("dropdown-content-nav");
+        let i;
         for (i = 0; i < dropdowns.length; i++) {
-            var openDropdown = dropdowns[i];
+            let openDropdown = dropdowns[i];
             if (openDropdown.classList.contains('show-nav')) {
                 openDropdown.classList.remove('show-nav');
             }
@@ -76,38 +69,127 @@ window.onclick = function (event) {
 
 function myFunction5() {
     document.getElementById("myDropdown-nav5").classList.toggle("show-nav");
+    document.getElementById("myDropdown-nav2").classList.remove("show-nav");
+    document.getElementById("myDropdown-nav3").classList.remove("show-nav");
+    document.getElementById("myDropdown-nav4").classList.remove("show-nav");
+    document.getElementById("myDropdown-nav6").classList.remove("show-nav");
 }
 
 // Close the dropdown if the user clicks outside of it
 window.onclick = function (event) {
     if (!event.target.matches('.dropbtn-nav')) {
 
-        var dropdowns = document.getElementsByClassName("dropdown-content-nav");
-        var i;
+        let dropdowns = document.getElementsByClassName("dropdown-content-nav");
+        let i;
         for (i = 0; i < dropdowns.length; i++) {
-            var openDropdown = dropdowns[i];
+            let openDropdown = dropdowns[i];
             if (openDropdown.classList.contains('show-nav')) {
                 openDropdown.classList.remove('show-nav');
             }
+
         }
     }
 }
 
 function myFunction6() {
     document.getElementById("myDropdown-nav6").classList.toggle("show-nav");
+    document.getElementById("myDropdown-nav2").classList.remove("show-nav");
+    document.getElementById("myDropdown-nav3").classList.remove("show-nav");
+    document.getElementById("myDropdown-nav4").classList.remove("show-nav");
+    document.getElementById("myDropdown-nav5").classList.remove("show-nav");
 }
 
 // Close the dropdown if the user clicks outside of it
 window.onclick = function (event) {
     if (!event.target.matches('.dropbtn-nav')) {
 
-        var dropdowns = document.getElementsByClassName("dropdown-content-nav");
-        var i;
+        let dropdowns = document.getElementsByClassName("dropdown-content-nav");
+        let i;
         for (i = 0; i < dropdowns.length; i++) {
-            var openDropdown = dropdowns[i];
+            let openDropdown = dropdowns[i];
             if (openDropdown.classList.contains('show-nav')) {
                 openDropdown.classList.remove('show-nav');
             }
+            openDropdown.classList.remove('show-nav');
+        }
+    }
+}
+
+function myFunction7() {
+    document.getElementById("myDropdown-nav7").classList.toggle("show-nav");
+    document.getElementById("myDropdown-nav2").classList.remove("show-nav");
+    document.getElementById("myDropdown-nav3").classList.remove("show-nav");
+    document.getElementById("myDropdown-nav4").classList.remove("show-nav");
+    document.getElementById("myDropdown-nav6").classList.remove("show-nav");
+    document.getElementById("myDropdown-nav7").classList.remove("show-nav");
+    document.getElementById("myDropdown-nav8").classList.remove("show-nav");
+    document.getElementById("myDropdown-nav9").classList.remove("show-nav");
+}
+
+// Close the dropdown if the user clicks outside of it
+window.onclick = function (event) {
+    if (!event.target.matches('.dropbtn-nav')) {
+
+        let dropdowns = document.getElementsByClassName("dropdown-content-nav");
+        let i;
+        for (i = 0; i < dropdowns.length; i++) {
+            let openDropdown = dropdowns[i];
+            if (openDropdown.classList.contains('show-nav')) {
+                openDropdown.classList.remove('show-nav');
+            }
+            openDropdown.classList.remove('show-nav');
+        }
+    }
+}
+
+function myFunction8() {
+    document.getElementById("myDropdown-nav8").classList.toggle("show-nav");
+    document.getElementById("myDropdown-nav2").classList.remove("show-nav");
+    document.getElementById("myDropdown-nav3").classList.remove("show-nav");
+    document.getElementById("myDropdown-nav4").classList.remove("show-nav");
+    document.getElementById("myDropdown-nav6").classList.remove("show-nav");
+    document.getElementById("myDropdown-nav7").classList.remove("show-nav");
+    document.getElementById("myDropdown-nav8").classList.remove("show-nav");
+    document.getElementById("myDropdown-nav9").classList.remove("show-nav");
+}
+
+// Close the dropdown if the user clicks outside of it
+window.onclick = function (event) {
+    if (!event.target.matches('.dropbtn-nav')) {
+
+        let dropdowns = document.getElementsByClassName("dropdown-content-nav");
+        let i;
+        for (i = 0; i < dropdowns.length; i++) {
+            let openDropdown = dropdowns[i];
+            if (openDropdown.classList.contains('show-nav')) {
+                openDropdown.classList.remove('show-nav');
+            }
+            openDropdown.classList.remove('show-nav');
+        }
+    }
+}
+
+function myFunction9() {
+    document.getElementById("myDropdown-nav9").classList.toggle("show-nav");
+    document.getElementById("myDropdown-nav2").classList.remove("show-nav");
+    document.getElementById("myDropdown-nav3").classList.remove("show-nav");
+    document.getElementById("myDropdown-nav4").classList.remove("show-nav");
+    document.getElementById("myDropdown-nav6").classList.remove("show-nav");
+    document.getElementById("myDropdown-nav7").classList.remove("show-nav");
+}
+
+// Close the dropdown if the user clicks outside of it
+window.onclick = function (event) {
+    if (!event.target.matches('.dropbtn-nav')) {
+
+        let dropdowns = document.getElementsByClassName("dropdown-content-nav");
+        let i;
+        for (i = 0; i < dropdowns.length; i++) {
+            let openDropdown = dropdowns[i];
+            if (openDropdown.classList.contains('show-nav')) {
+                openDropdown.classList.remove('show-nav');
+            }
+            openDropdown.classList.remove('show-nav');
         }
     }
 }
